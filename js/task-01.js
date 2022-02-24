1ce083dc832b9e9a79a008832ca0063ab07ff3e6
wojciechonoszko <wojciechonoszko@gmail.com>
2022-02-24 09:35:25 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -6,5 +6,15 @@ for (let i=0; i<$ul.childNodes.length; i++){
         liNodes.push($ul.childNodes[i]);
     }
 }
-console.log(`Number of categories: ${liNodes.length}`);
+console.log(`Number of categories: ${liNodes.length} `);
 
+// console.log($ul.children[0].children[1].children.length)
+
+// console.log($ul.firstChild.childNodes);
+
+// console.log($ul.firstElementChild.children);
+// console.log(document.getElementsByTagName('h2')[0].textContent);
+// console.log(document.getElementsByTagName('')[0]);
+console.log(' ');
+console.log(`Category: ${document.getElementsByTagName('h2')[0].textContent}`);
+console.log(`Elements: ${$ul.children[0].children[1].children.length}`)
