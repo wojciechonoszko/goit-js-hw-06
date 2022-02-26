42acf5fff9621a1a2b9644052bda3cc71604184c
wojciechonoszko <wojciechonoszko@gmail.com>
2022-02-26 15:28:08 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,15 @@
 const $ul = document.getElementById("categories");
-const liNodes = [];
 
-for (let i=0; i<$ul.childNodes.length; i++){
-    if ($ul.childNodes[i].nodeName == "LI") {
-        liNodes.push($ul.childNodes[i]);
-    }
-}
-console.log(`Number of categories: ${liNodes.length} `);
+console.log(`Number of categories: ${$ul.children.length}`)
 
-// console.log($ul.children[0].children[1].children.length)
-
-// console.log($ul.firstChild.childNodes);
-
-// console.log($ul.firstElementChild.children);
-// console.log(document.getElementsByTagName('h2')[0].textContent);
-// console.log(document.getElementsByTagName('')[0]);
 console.log(' ');
 console.log(`Category: ${document.getElementsByTagName('h2')[0].textContent}`);
 console.log(`Elements: ${$ul.children[0].children[1].children.length}`)
+
+console.log(' ');
+console.log(`Category: ${document.getElementsByTagName('h2')[1].textContent}`);
+console.log(`Elements: ${$ul.children[1].children[1].children.length}`);
+
+console.log(' ');
+console.log(`Category: ${document.getElementsByTagName('h2')[2].textContent}`);
+console.log(`Elements: ${$ul.children[2].children[1].children.length}`)
