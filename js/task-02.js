42a145079b9e04cc6284199c4bc02053945b5d36
wojciechonoszko <wojciechonoszko@gmail.com>
2022-02-26 16:41:55 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const list = document.createDocumentList();
+for (let i=0; i<ingredients.length; i++){
+  const $li = document.createElement("li");
+  $li.textContent = ingredients[i];
+  $li.classList.add("item");
+
+  document.body.append($li);
+}
