0f0dc882be94cf2069e88de5274b2fe60ca390b3
wojciechonoszko <wojciechonoszko@gmail.com>
2022-02-28 18:41:35 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
+
+const email = document.querySelector("[type=email]");
+
+const password = document.querySelector("[type=password]");
+
+
+const submitForm = (ev) => {
+    const form = ev.currentTarget;
+    ev.preventDefault()
+    const{
+        elements: {email, password}, 
+    } = form;
+
+    if ((!email.value) || (!password.value)){
+        return alert("You must fill in all fields in form.")
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    form.reset();
+}
+
+form.addEventListener("submit", submitForm);
+
 
