fd15ab45310fd1e9edf6408e42169b2852e158e3
wojciechonoszko <wojciechonoszko@gmail.com>
2022-02-28 09:22:21 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,8 @@
 const inputName = document.querySelector("#name-input");
 const outputName = document.querySelector("#name-output");
 
-console.log(inputName.textContent.length);
-console.log(outputName);
 
 inputName.addEventListener("input", (event) => {
-    if (inputName.textContent.length){
-        outputName.textContent = "anonym";
-    }
-    else{
-    outputName.textContent = event.currentTarget.value;
-    }
+        outputName.textContent = event.currentTarget.value;
+         
 })
