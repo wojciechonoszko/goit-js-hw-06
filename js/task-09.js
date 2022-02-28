26bb9ae6cba053bdf28d1065cab869000ba61a9a
wojciechonoszko <wojciechonoszko@gmail.com>
2022-02-28 19:12:14 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanBackground = document.querySelector(".color");
+
+const changeColor = document.querySelector(".change-color");
+
+const buttonChangeColor = (ev) => {
+  spanBackground.style.backgroundColor = getRandomHexColor();
+}
+
+addEventListener("click", buttonChangeColor);
